6b8e3bd86b314edb5a371d04f89a7969a5ca73ed
CoryHershman <36083724+CoryHershman@users.noreply.github.com>
2018-02-02 22:51:23 -0500

    HelloWorld file added
    The HelloWorld file was added to show off the awesome powers of git.

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld
+{
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+    System.out.println("I will use these git powers to become the emperor god-king");
+  }
+}
